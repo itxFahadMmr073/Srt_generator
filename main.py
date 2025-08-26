56950f3eabfd0ef5f19147c3ddfab76ff938eeb8
itxFahadMmr073 <khanfahad004x@gmail.com>
2025-08-26 22:24:18 +0530

    file_saved

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ a = "apple"
 b = "ball"
 c = a +b
 print(c)
+
+age = int(input("what your age"))
+print = (age)
+# // im not unable to make a simple pythong
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -8,11 +8,11 @@ print = (age)
  
 #  copy version
 
-a = "apple"
-b = "ball"
-c = a +b
-print(c)
+# a = "apple"
+# b = "ball"
+# c = a +bgit
+# print(c)
 
-age = int(input("what your age"))
-print = (age)
+# age = int(input("what your age"))
+# print = (age)
  
